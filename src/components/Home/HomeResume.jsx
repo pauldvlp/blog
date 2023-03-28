e7ab9569fc32f61e776cbd2dfd818ac15040fabb
Paul Barahona <p4ul.dev@gmail.com>
2023-03-28 12:32:33 -0600

    Updated

--- src/components/Home/HomeResume.jsx
+++ src/components/Home/HomeResume.jsx
@@ -4,10 +4,10 @@ import React from "react";
 const HomeResume = () => {
   return (
     <div className="text-center max-w-3xl text-slate-800">
-      <p>
+      <p className='text-lg'>
         Soy <strong>Desarrollador y Diseñador Web y UI</strong> de Honduras.
       </p>
-      <p className="sm:text-sm">
+      <p>
         Este es mi blog personal donde te compartire sobre temas de tecnologia, y, tambien experiencias personales, esperando que pueda ayudarte.
         Si quieres que escriba sobre un tema puedes <Link className="text-blue-500 border-b border-b-blue-500" href={'#contact'}>Contactarme</Link> y decirmelo.
       </p>
